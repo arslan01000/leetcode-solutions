49bc9af0fa97951a707855073c65df9d30c3dc5b
Arslan <127810063+arslan01000@users.noreply.github.com>
2025-02-05 23:06:11 -0600

    Commented 193-Valid-Phone-Numbers.sh

--- Shell/193-Valid-Phone-Numbers.sh
+++ Shell/193-Valid-Phone-Numbers.sh
@@ -2,5 +2,6 @@
 grep -E '^(\([0-9]{3}\) [0-9]{3}-[0-9]{4}|[0-9]{3}-[0-9]{3}-[0-9]{4})$' file.txt
 
 
+
 # This script filters valid phone numbers from file.txt
 # Valid formats: (xxx) xxx-xxxx or xxx-xxx-xxxx
